34746a4d51a07aad6687a6ff6b756e49ad753023
ChaeYeon Kim <soqk127@naver.com>
2022-11-17 18:32:01 +0900

    Category and topButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { TopNavigationBar } from "./components/header/topNavigationBar";
+import Home from "./pages/home";
+import Product from "./pages/product";
+import Basket from "./pages/basket";
+import TopButton from "./components/topButton/topButton";
+import CategoryBar from "./components/category/categoryBar";
+import { CategoryMove } from "./components/category/categoryMove";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const convertPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
   return (
-    <div className="App">
-      Hello, React!
-    </div>
+    <BrowserRouter>
+      <TopNavigationBar cart={cart} />
+      <CategoryBar></CategoryBar>
+      <TopButton></TopButton>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              convertPrice={convertPrice}
+              products={products}
+              setProducts={setProducts}
+            />
+          }
+        />
+        <Route
+          path="/product/:id"
+          element={
+            <Product
+              convertPrice={convertPrice}
+              cart={cart}
+              setCart={setCart}
+            />
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Basket cart={cart} setCart={setCart} convertPrice={convertPrice} />
+          }
+        />
+        <Route
+          path="/category/:n"
+          element={
+            <CategoryMove
+              convertPrice={convertPrice}
+              products={products}
+              setProducts={setProducts}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/category/categoryBar.js
@@ -0,0 +1,36 @@
+import Nav from 'react-bootstrap/Nav';
+
+function CategoryBar() {
+    return (
+        <>
+        <Nav className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link href="/category/c1" className="text-black">디자인문구</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c2" className="text-black">디지털/핸드폰</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c3" className="text-black">가구/수납</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c4" className="text-black">데코/조명</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c5" className="text-black">키친</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c6" className="text-black">패션의류</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c7" className="text-black">뷰티</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/category/c8" className="text-black">주얼리/시계</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </>
+      );
+}
+
+export default CategoryBar;
