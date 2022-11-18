5a1c0c603eae7d1e4c2679fadd5cf93b9c4831f7
minji <ipink89@naver.com>
2022-11-19 00:32:12 +0900

    1119_1231

--- src/components/showname/showname.js
+++ src/components/showname/showname.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import styles from "./showname.module.css";
 
 class ShowName extends Component {
   state = {
@@ -6,6 +9,10 @@ class ShowName extends Component {
     name: ''
   }
   handleChange = (e) => {
+    if (e.target.value.length > 5) {
+      alert('5글자까지 입력할수있습니다.');
+      return;
+    }
     this.setState({
       [e.target.name]: e.target.value //입력값을 받아옴
     })
@@ -20,32 +27,37 @@ class ShowName extends Component {
       name: ''
     })
   }
-  handleClick= (e) => {
+  handleClick= () => {
+    if (this.state.name.length === 0) {
+      alert('이름을 입력하세요! ');
+      return;
+    }
     this.setState({
         isSubmit: true
     })
   }
   clickUpdate= (e) => {
     this.setState({
-      isSubmit: false
+      isSubmit: false,
+      name: ''
   })
   }
   render() {
     return (
-      <div style={{width: '100px'}}>
-        <div hidden = {this.state.isSubmit}>
-          <input style={{display: 'inline'}}
+      <div>
+        <div className={styles.menu} hidden = {this.state.isSubmit}>
+          <Form.Control style={{ marginRight: '8px', marginLeft: '100px'}}
           placeholder="이름"
           value={this.state.name}
           onChange={this.handleChange}
-          name="name"
+          name="name"          
         />
-        <button onClick={this.handleClick}>등록</button>
+        <Button style={{width:'100px'}} onClick={this.handleClick} variant="outline-primary">Submit</Button>
         </div>
 
         <div hidden = {!this.state.isSubmit}> 
-          <span style={{ marginRight: '8px'}}>{this.state.name} 아아아아</span>
-          <button onClick={this.clickUpdate}>수정</button>
+          <span style={{ marginRight: '8px'}}>'{this.state.name}' 님!  환영합니다!</span>
+          <Button onClick={this.clickUpdate} variant="outline-primary">Edit</Button>
         </div>
       </div>
     );
