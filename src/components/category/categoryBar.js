8308d276f72731bf6ae2afb0e16c3b2ed59aa1f3
ChaeYeon Kim <soqk127@naver.com>
2022-11-19 21:32:07 +0900

    categorybar modified

--- src/components/category/categoryBar.js
+++ src/components/category/categoryBar.js
@@ -13,10 +13,11 @@ const StyledLink = styled(Link)`
   margin: 0 auto;
   text-align: center;
   text-decoration:none;
-  border-radius: 8px;
+  border-radius: 10px;
   line-height: 1.8;
   border: 1px solid lightgray;
   font-weight: ${(props) => (props.isActive ? "bold" : "normal")};
+  color: ${(props) => (props.isActive ? "rgb(25,103,206)" : "black")};
 `;
 
 function CategoryBar() {
@@ -26,28 +27,28 @@ function CategoryBar() {
       <>
         <Nav justify variant="tabs">
           <Nav.Item>
-            <StyledLink to="/category/c1" className="text-black" isActive={pathname === "/category/c1"}>디자인문구</StyledLink>
+            <StyledLink to="/category/c1" isActive={pathname === "/category/c1"}>디자인문구</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c2" className="text-black" isActive={pathname === "/category/c2"}>디지털/핸드폰</StyledLink>
+            <StyledLink to="/category/c2" isActive={pathname === "/category/c2"}>디지털/핸드폰</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c3" className="text-black" isActive={pathname === "/category/c3"}>가구/수납</StyledLink>
+            <StyledLink to="/category/c3" isActive={pathname === "/category/c3"}>가구/수납</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c4" className="text-black" isActive={pathname === "/category/c4"}>데코/조명</StyledLink>
+            <StyledLink to="/category/c4" isActive={pathname === "/category/c4"}>데코/조명</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c5" className="text-black" isActive={pathname === "/category/c5"}>키친</StyledLink>
+            <StyledLink to="/category/c5" isActive={pathname === "/category/c5"}>키친</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c6" className="text-black" isActive={pathname === "/category/c6"}>패션잡화</StyledLink>
+            <StyledLink to="/category/c6" isActive={pathname === "/category/c6"}>패션잡화</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c7" className="text-black" isActive={pathname === "/category/c7"}>뷰티</StyledLink>
+            <StyledLink to="/category/c7" isActive={pathname === "/category/c7"}>뷰티</StyledLink>
           </Nav.Item>
           <Nav.Item>
-            <StyledLink to="/category/c8" className="text-black" isActive={pathname === "/category/c8"}>주얼리/시계</StyledLink>
+            <StyledLink to="/category/c8" isActive={pathname === "/category/c8"}>주얼리/시계</StyledLink>
           </Nav.Item>
         </Nav>
       </>
